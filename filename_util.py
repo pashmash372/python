e400332cc3bbd6b0ce360da69189b30e728ddd5a
pashmash372 <51986499+pashmash372@users.noreply.github.com>
2025-02-13 20:26:28 +0530

    Section 12-Mathematics Practice Questions branch created

--- filename_util.py
+++ filename_util.py
@@ -2,12 +2,13 @@ import sys
 
 
 def remove_spaces_and_replace_colon_with_hyphen(string):
+    print("String before processing: ", string)
     return string.replace(" ", "").replace(":", "-") + ".ipynb"
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: python remove_spaces_and_replace_colon_with_hyphen.py <input_string>")
     else:
-        input_string = sys.argv[1]
+        input_string = "".join(sys.argv[1::])
         print(remove_spaces_and_replace_colon_with_hyphen(input_string))
